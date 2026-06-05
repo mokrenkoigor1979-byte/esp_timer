14c5702b2c433431e7e96abc489f7a8ea06c82d3
aton_5 <mokrenkoigor1979@gmail.com>
2026-06-05 22:49:54 +0300

    создать и запустить timer

--- main/main.c
+++ main/main.c
@@ -18,4 +18,11 @@ void app_main(void)
     gpio_reset_pin(LED);
     gpio_set_direction(LED, GPIO_MODE_OUTPUT);
     gpio_set_level(LED, state);
+
+    esp_timer_handle_t timer;
+    esp_timer_create_args_t timer_args = {
+        .callback = timer_callback,
+        .name = "oneshot"};
+    esp_timer_create(&timer_args, &timer);
+    esp_timer_start_periodic(timer, 1000000);
 }
