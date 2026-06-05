e869e6ede51ac46bb0f42228089ce74ada420da2
aton_5 <mokrenkoigor1979@gmail.com>
2026-06-05 22:46:18 +0300

    насройка светодиода пин 21

--- main/main.c
+++ main/main.c
@@ -1,5 +1,15 @@
-#include <stdio.h>
+#include <stdbool.h>
+
+#include "driver/gpio.h"
+#include "esp_timer.h"
+
+#define LED GPIO_NUM_21
+
+static bool state = false;
 
 void app_main(void)
 {
+    gpio_reset_pin(LED);
+    gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+    gpio_set_level(LED, state);
 }
